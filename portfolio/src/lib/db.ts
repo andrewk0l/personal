ca2ed3ff7fb7c4fb08a03ee1e95a6c331d49b239
oluwanifemi <oluwanifemi.ogedengbe@ollasystems.com>
2026-04-30 20:24:48 +0100

    db connection string

--- portfolio/src/lib/db.ts
+++ portfolio/src/lib/db.ts
@@ -1,6 +1,7 @@
 ﻿import postgres from 'postgres';
 
-export const sql = postgres(process.env.POSTGRES_URL!);
+const connectionString = process.env.POSTGRES_URL || process.env.DATABASE_URL;
+export const sql = postgres(connectionString!);
 
 export type Post = {
     id: number;
