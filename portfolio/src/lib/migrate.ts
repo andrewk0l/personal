8681314d73c1f50b4f44abb9583fde88e383a3cc
oluwanifemi-ollasystems <oluwanifemi.ogedengbe@ollasystems.com>
2026-04-30 12:45:34 +0100

    created db migrations for blog section

---
+++ portfolio/src/lib/migrate.ts
@@ -0,0 +1,25 @@
+﻿import 'dotenv/config'
+console.log('DB URL:', process.env.POSTGRES_URL)
+
+import { sql } from './db';
+
+
+async function migrate() {
+    try {
+        await sql`
+      CREATE TABLE IF NOT EXISTS posts (
+        id SERIAL PRIMARY KEY,
+        title TEXT NOT NULL,
+        content TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT NOW()
+      )
+    `;
+        console.log('Migration complete');
+    } catch (error) {
+        console.error('Migration failed:', error);
+    } finally {
+        process.exit(0);
+    }
+}
+
+migrate();
